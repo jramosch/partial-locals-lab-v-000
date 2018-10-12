ea3d41cfe5b70c8e16baa7745ff1f4e4892604dc
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-12 14:04:10 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(search)
-    where("name LIKE ?", "%@{search}%")
+  def self.search(query)
+    where("name LIKE ?", "%@{query}%")
   end
 end
